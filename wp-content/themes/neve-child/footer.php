43c8922728ea4f9bf5decc24c54c6a60a5ca6a9e
Marcelo Bursztein <mbursztein@gmail.com>
2020-04-22 21:34:12 -0400

    LinkedIn tracking tag

--- wp-content/themes/neve-child/footer.php
+++ wp-content/themes/neve-child/footer.php
@@ -31,5 +31,6 @@ if ( apply_filters( 'neve_filter_toggle_content_parts', true, 'footer' ) === tru
 	<a href="/privacy-policy">Privacy Policy</a>
 </div>
 
-
+	
+<script type="text/javascript"> _linkedin_partner_id = "1941042"; window._linkedin_data_partner_ids = window._linkedin_data_partner_ids || []; window._linkedin_data_partner_ids.push(_linkedin_partner_id); </script><script type="text/javascript"> (function(){var s = document.getElementsByTagName("script")[0]; var b = document.createElement("script"); b.type = "text/javascript";b.async = true; b.src = "https://snap.licdn.com/li.lms-analytics/insight.min.js"; s.parentNode.insertBefore(b, s);})(); </script> <noscript> <img height="1" width="1" style="display:none;" alt="" src="https://px.ads.linkedin.com/collect/?pid=1941042&fmt=gif" /> </noscript>
 </body>
